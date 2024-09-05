18a12bb2d677e847b9da721d2bbe2019d37efa12
Akhmadovich03 <akhmadovich03@gmail.com>
2024-09-05 20:58:43 +0500

    Removed confidential datas

--- CollectionManagement/Models/Constants.cs
+++ CollectionManagement/Models/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const string JIRA_DOMAIN = "collectionapp.atlassian.net";
-    public const string JIRA_EMAIL = "akhmadovich03@gmail.com";
-    public const string JIRA_API_TOKEN = "ATATT3xFfGF07Ko3qupTlAlIlIkvW-3q0Cw0VyJsOoyCxxaExmcWRlTfk0mJ2tuQPkspR9oWe7TBZfLURlCmGr4P82OHTYQSsyt2OBtdIjarw1xf-ZP1IV1M5cSQsQ8aMWLOnMc3jKwhySwFOuR9wGFu5_IQ05MhcAYTWV76PZzAQrgg3th_bXM=5053F1A8";
+    public const string JIRA_DOMAIN = "your_jira_domain";
+    public const string JIRA_EMAIL = "your_jira_email";
+    public const string JIRA_API_TOKEN = "your_jira_api_token";
 }
